f6f514195c7ffd81f4de54a355ec8ba4ade055fa
sg5282 <sg5282@rit.edu>
2026-06-14 17:46:17 -0400

    Changed to add things from Victor's branch

--- ufund-api/src/test/java/com/ufund/api/ufundapi/controller/NeedControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/NeedControllerTest.java
@@ -1,4 +1,4 @@
-package com.ufund.api.ufundapi.controller;
+package com.ufund.api.ufundapi.Controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doThrow;
@@ -7,8 +7,7 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
-import com.ufund.api.ufundapi.model.Need;
-import com.ufund.api.ufundapi.service.NeedService;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -16,62 +15,40 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.ufund.api.ufundapi.controller.NeedController;
+import com.ufund.api.ufundapi.dao.NeedDAO;
+import com.ufund.api.ufundapi.model.Need;
 
+/**
+ * Test the NeedController Class
+ * 
+ */
 @Tag("Controller-tier")
 public class NeedControllerTest {
     private NeedController needController;
-    private NeedService needService;
+    private NeedDAO mockneedDAO;
 
-        /**
-     * Before each test, create a new NeedController object and inject
-     * a mock Need Service
+    /**
+     * Before each test, create a new controller
+     * additionally, create a mock DAO
      */
+
     @BeforeEach
-    public void setupNeedController() {
-        needService = mock(NeedService.class);
-        needController = new NeedController(needService);
+    public void setupNeedController(){
+        mockneedDAO = mock(NeedDAO.class);
+        needController = new NeedController(mockneedDAO);
     }
 
-    @Test 
-    public void testUpdateNeed() throws Exception{  // getNeed may throw IOException
-        Need need = new Need(1, "Corn", 10.97, 100, "food");
-        when(needService.updateNeed(need.getId(), need)).thenReturn(need);
+    @Test
+    public void testCreateNeed() throws IOException {
+        Need need = new Need();
 
-        ResponseEntity<Need> response = needController.updateNeed(need.getId(), need);
+        when(mockneedDAO.createNeed(need)).thenReturn(need);
 
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(need, response.getBody());
+        ResponseEntity<Need> response = needController.createNeed(need);
+
+        assertEquals(HttpStatus.CREATED,response.getStatusCode());
+        assertEquals(need,response.getBody());
     }
 
-    @Test 
-    public void testGetNeedFailed() throws Exception {  // getNeed may throw IOException
-        // Setup
-        Need need = new Need(3, "Corn", 10.97, 100, "food");
-
-
-        // when updateNeed is called, return null simulating failed
-        // update and save
-        when(needService.updateNeed(need.getId(), need)).thenReturn(null);
-
-        // Invoke
-        ResponseEntity<Need> response = needController.updateNeed(need.getId(), need);
-
-        // Analyze
-        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
-    }
-
-    @Test void testUpdateNeedHandleException() throws Exception {
-        // Setup
-        Need need = new Need(3, "Corn", 10.97, 100, "food");
-
-        // When updateNeed is called on the Mock Need Service, throw an IOException
-        doThrow(new IOException()).when(needService).updateNeed(need.getId(), need);
-
-        // Invoke
-        ResponseEntity<Need> response = needController.updateNeed(need.getId(), need);
-
-        // Analyze
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-    }
 }
