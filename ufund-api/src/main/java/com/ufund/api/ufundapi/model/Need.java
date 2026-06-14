5ffd95356a6690f8eaeb7b1f25e98f951b599549
Victor Omolo <MossSizzle@gmail.com>
2026-06-14 06:18:14 -0600

    Added NEED entity

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -0,0 +1,49 @@
+package com.ufund.api.ufundapi.model;
+
+public class Need{
+    private int id;
+    private String name;
+    private int quantity;
+    private String unit;
+
+    public Need() {}
+
+    public Need(int id, String name, int quantity, String unit) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
