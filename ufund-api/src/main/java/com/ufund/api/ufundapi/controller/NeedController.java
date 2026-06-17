a312c80ea5a1ed7234b031680548ca635a68a93e
Pedrocia De-Sosoo <psd4724@g.rit.edu>
2026-06-17 19:38:29 -0400

    Fixed case-insensitive search,and added blank validation

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
@@ -65,8 +65,11 @@ public class NeedController {
         LOG.info("GET /needs" + (name != null ? "?name=" + name : ""));
         try {
             List<Need> needs;
-            if (name != null)
+            if (name != null) {
+                if (name.isBlank())
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 needs = needService.findNeeds(name);
+            }
             else
                 needs = needService.getAllNeeds();
             return new ResponseEntity<>(needs.toArray(new Need[0]), HttpStatus.OK);
