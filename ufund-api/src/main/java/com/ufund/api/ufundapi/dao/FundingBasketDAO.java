652032cfc8649e4ffdf7eb2226bff8cae683b577
sg5282 <sg5282@rit.edu>
2026-07-02 00:18:45 -0400

    Added FundingBasketFileDAO and its' rough implementation. Will revise as needed later.

--- ufund-api/src/main/java/com/ufund/api/ufundapi/dao/FundingBasketDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/dao/FundingBasketDAO.java
@@ -13,25 +13,23 @@ public interface FundingBasketDAO {
      * @return {@link FundingBasket fb} the Funding basket with the same id
      */
     FundingBasket getFundingBasket(int id);
+
+    FundingBasket[] getFundingBasketArray();
     
     /**
      * Creates a new {@linkplain FundingBasket fb}
-     * @param fb {@link FundingBasket} the FundingBasket to be created
-     * @return new {@link FundingBasket} if successful, null if otherwise
+     * @param fb {@link FundingBasket fb} the FundingBasket to be created
+     * @return new {@link FundingBasket fb} if successful, null if otherwise
      * @throws IOException if an issue with storage occurs
      */
     FundingBasket createFundingBasket(FundingBasket fb) throws IOException;
 
     /**
-     * 
-     * @param id
-     * @return
-     * @throws IOException
+     * Deletes an existing {@linkplain FundingBasket fb}
+     * @param id {@link int id} the id of the FundingBasket for deletion
+     * @return true if successful, false if failure
+     * @throws IOException if an issue with storage access occurs
      */
-    FundingBasket deleteFundingBasket(int id) throws IOException;
-
-    Funding
-
-
+    boolean deleteFundingBasket(int id) throws IOException;
 
 }
