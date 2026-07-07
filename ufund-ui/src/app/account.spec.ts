527c9cf5db77409afc13f81e2f08698cda412bde
Pedrocia De-Sosoo <psd4724@g.rit.edu>
2026-07-07 11:55:02 -0400

    Resolved merge conflicts in needs.json and account.spec.ts

--- ufund-ui/src/app/account.spec.ts
+++ ufund-ui/src/app/account.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Account } from './account';
+import { AccountService } from './account';
 
-describe('Account', () => {
-  let service: Account;
+describe('AccountService', () => {
+  let service: AccountService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Account);
+    service = TestBed.inject(AccountService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should start with no one logged in', () => {
+    expect(service.isLoggedIn()).toBeFalse();
+    expect(service.getCurrentAccount()).toBeNull();
   });
 });
