920e271b95b0ea96e6b7da0c12064ffbf78179f3
lsparakis <hs5980@g.rit.edu>
2026-07-28 16:37:58 -0400

    acct unit tests

--- ufund-api/src/test/java/com/ufund/api/ufundapi/controller/AccountControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/AccountControllerTest.java
@@ -1,16 +1,16 @@
 package com.ufund.api.ufundapi.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -120,6 +120,13 @@ public class AccountControllerTest {
         assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
         body.replace("password", "12345");
 
+    }
+
+    @Test
+    public void testLoginException() throws Exception {
+        doThrow(new IOException()).when(accountService).getAccount("charlieg");
+        ResponseEntity<Account> response = accountController.login(body);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 
 }
